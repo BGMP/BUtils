a4bbec03d412b01f35c6cc56cc3b5da4386f4d53
José Benavente <jose@bgmp.cl>
2020-10-26 17:43:29 -0300

    Document SimpleDuration

--- src/main/java/cl/bgmp/butils/time/SimpleDuration.java
+++ src/main/java/cl/bgmp/butils/time/SimpleDuration.java
@@ -2,8 +2,16 @@ package cl.bgmp.butils.time;
 
 import java.time.Duration;
 
+/** Java {@link Duration}s, simplified. */
 public interface SimpleDuration {
 
+  /**
+   * Converts a humanised duration string, such as {@code 1m, 2h, 3d, etc.} into a {@link Duration}.
+   *
+   * @param s The humanised duration string.
+   * @return The passed string parsed to a {@link Duration}.
+   * @throws java.time.format.DateTimeParseException if the text cannot be parsed to a Duration.
+   */
   static Duration fromString(String s) {
     return Duration.parse("PT" + s);
   }
